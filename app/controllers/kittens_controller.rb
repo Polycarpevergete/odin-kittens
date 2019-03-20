b762250c528c867fa812e02a3d3d70fb0fc83c81
polycarpevergete <xavier.sassi@gmail.com>
2019-03-20 11:38:37 +0700

    all functional kitten box creator

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,43 @@
 class KittensController < ApplicationController
+    def index 
+        @kittens = Kitten.all
+    end
+
+    def new 
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.create(kitten_params)
+        flash[:success] = "New kitten created"
+        redirect_to root_path
+    end
+
+    def show
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def edit 
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def update 
+        @kitten = Kitten.find(params[:id])
+        @kitten.update_attributes(kitten_params)
+        flash[:update] = "Kitten updated"
+        redirect_to root_path
+    end
+
+    def destroy
+        @kitten = Kitten.find(params[:id])
+        @kitten.delete
+        flash[:success] = "Kitten deleted"
+        redirect_to root_path
+    end
+
+    private
+
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 end
